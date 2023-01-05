8655f207ed85432366acc9c99736530e5d9485c2
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-01-05 15:12:40 +0530

    project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'simple-counter';
+
+  counter = 0
+
+  incDec(type: string) {
+    (type=='subtract') ? this.counter-- : this.counter++
+  }
+
 }
